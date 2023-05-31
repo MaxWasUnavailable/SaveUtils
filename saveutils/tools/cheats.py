ccfb646d4a12ffadb5b94c551a66a0e45ed2382d
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-05-31 16:30:01 +0200

    Add argparse subparser & arg handler for cheats
    Signed-off-by: Max <89798523+MaxWasUnavailable@users.noreply.github.com>

--- saveutils/tools/cheats.py
+++ saveutils/tools/cheats.py
@@ -55,6 +55,7 @@ class HealthCheats:
     """
     A collection of health cheats.
     """
+
     @staticmethod
     def set_health(save: SaveFile, amount: int):
         """
@@ -88,3 +89,63 @@ class HealthCheats:
         """
         current_health = save.get_health()
         logger.info(f"Current health: {current_health}")
+
+
+# Add the cheats subparser
+def register_subparser(main_parser) -> None:
+    """
+    Registers the cheats subparser.
+    :param main_parser: The main parser
+    """
+    cheats_parser = main_parser.add_subparsers(title="Cheats", description=description, dest="cheats")
+
+    money_parser = cheats_parser.add_parser("money", help="Money cheats")
+    money_parser.add_argument("amount", type=int, help="The amount of money to add or remove")
+    money_parser.add_argument("-a", "--add", action="store_true", help="Add money to your character's bank account")
+    money_parser.add_argument("-r", "--remove", action="store_true",
+                              help="Remove money from your character's bank account")
+    money_parser.add_argument("-s", "--set", action="store_true", help="Set the amount of money your character has")
+    money_parser.add_argument("-p", "--print", action="store_true",
+                              help="Print the current amount of money your character has")
+
+    health_parser = cheats_parser.add_parser("health", help="Health cheats")
+    health_parser.add_argument("amount", type=float, help="The amount of health to add or remove")
+    health_parser.add_argument("-a", "--add", action="store_true", help="Add health to your character's health")
+    health_parser.add_argument("-r", "--remove", action="store_true", help="Remove health from your character's health")
+    health_parser.add_argument("-s", "--set", action="store_true", help="Set the amount of health your character has")
+    health_parser.add_argument("-p", "--print", action="store_true",
+                               help="Print the current amount of health your character has")
+
+
+# Handler for the cheats subparser
+def handle_subparser(args) -> None:
+    """
+    Handles the cheats subparser.
+    :param args: The arguments
+    """
+    save = SaveFile(args.save)
+
+    if args.cheats == "money":
+        if args.add:
+            MoneyCheats.add_money(save, args.amount)
+        elif args.remove:
+            MoneyCheats.remove_money(save, args.amount)
+        elif args.set:
+            MoneyCheats.set_money(save, args.amount)
+        elif args.print:
+            MoneyCheats.print_money(save)
+        else:
+            logger.error("No action specified. Use -a, -r, -s, or -p.")
+    elif args.cheats == "health":
+        if args.add:
+            HealthCheats.add_health(save, args.amount)
+        elif args.remove:
+            HealthCheats.remove_health(save, args.amount)
+        elif args.set:
+            HealthCheats.set_health(save, args.amount)
+        elif args.print:
+            HealthCheats.print_health(save)
+        else:
+            logger.error("No action specified. Use -a, -r, -s, or -p.")
+    else:
+        logger.error("No cheats specified. Use money or health.")
