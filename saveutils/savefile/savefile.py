4baf55531f2aab7d8fd1976b1cac2b965f62d54d
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-05-31 12:19:53 +0200

    Add getters for major save data
    Signed-off-by: Max <89798523+MaxWasUnavailable@users.noreply.github.com>

--- saveutils/savefile/savefile.py
+++ saveutils/savefile/savefile.py
@@ -3,7 +3,7 @@ import shutil
 from datetime import datetime
 from logging import getLogger, StreamHandler, Formatter
 from time import time
-from typing import Optional
+from typing import Optional, List
 
 
 class SaveFile:
@@ -171,12 +171,6 @@ class SaveFile:
         """
         return self.data.get("assignCaseID")
 
-    def get_murder_id(self) -> int:
-        """
-        Returns the murder ID of the save file.
-        """
-        return self.data.get("assignMurderID")
-
     def get_gamelength(self) -> int:
         """
         Returns the current game length.
@@ -195,8 +189,310 @@ class SaveFile:
         """
         return self.data.get("playerSurname")
 
+    def get_player_gender(self) -> int:
+        """
+        Get the player character's gender.
+        """
+        return self.data.get("playerGender")
+
+    def get_partner_gender(self) -> int:
+        """
+        Get the player character partner's gender.
+        """
+        return self.data.get("partnerGender")
+
+    def get_player_birthday(self) -> List[int]:
+        """
+        Returns the player character's birthday.
+
+        Format: [year, month, day]
+        """
+        return [self.data.get("playerBirthYear"), self.data.get("playerBirthMonth"), self.data.get("playerBirthDay")]
+
+    def get_residence(self) -> int:
+        """
+        Returns the player character's residence.
+        """
+        return self.data.get("residence")
+
+    def get_apartments_owned(self) -> List[int]:
+        """
+        Returns the player character's owned apartments.
+        """
+        return self.data.get("apartmentsOwned")
+
+    def is_tutorial_enabled(self) -> bool:
+        """
+        Returns whether the tutorial is enabled.
+        """
+        return self.data.get("tutorial")
+
+    def get_items(self) -> List[dict]:
+        """
+        Returns the player character's items.
+        """
+        return self.data.get("firstPersonItems")
+
+    def get_player_position(self) -> dict:
+        """
+        Returns the player character's position.
+        """
+        return self.data.get("playerPos")
+
+    def get_player_rotation(self) -> dict:
+        """
+        Returns the player character's rotation.
+        """
+        return self.data.get("playerRot")
+
     def get_money(self) -> int:
         """
         Returns the player's money.
         """
         return self.data.get("money")
+
+    def get_lockpicks(self) -> int:
+        """
+        Returns the player's lockpicks.
+        """
+        return self.data.get("lockpicks")
+
+    def get_social_credit(self) -> int:
+        """
+        Returns the player's social credit.
+        """
+        return self.data.get("socCredit")
+
+    def get_health(self) -> float:
+        """
+        Returns the player's health.
+        """
+        return self.data.get("health")
+
+    def get_nourishment(self) -> float:
+        """
+        Returns the player's nourishment.
+        """
+        return self.data.get("nourishment")
+
+    def get_hydration(self) -> float:
+        """
+        Returns the player's hydration.
+        """
+        return self.data.get("hydration")
+
+    def get_alertness(self) -> float:
+        """
+        Returns the player's alertness.
+        """
+        return self.data.get("alertness")
+
+    def get_energy(self) -> float:
+        """
+        Returns the player's energy.
+        """
+        return self.data.get("energy")
+
+    def get_hygiene(self) -> float:
+        """
+        Returns the player's hygiene.
+        """
+        return self.data.get("hygiene")
+
+    def get_heat(self) -> float:
+        """
+        Returns the player's heat.
+        """
+        return self.data.get("heat")
+
+    def get_drunk(self) -> float:
+        """
+        Returns the player's drunkness.
+        """
+        return self.data.get("drunk")
+
+    def get_sick(self) -> float:
+        """
+        Returns the player's sickness.
+        """
+        return self.data.get("sick")
+
+    def get_headache(self) -> float:
+        """
+        Returns the player's headache.
+        """
+        return self.data.get("headache")
+
+    def get_wet(self) -> float:
+        """
+        Returns the player's wetness.
+        """
+        return self.data.get("wet")
+
+    def get_broken_leg(self) -> float:
+        """
+        Returns the player's broken leg-ness.
+        """
+        return self.data.get("brokenLeg")
+
+    def get_bruised(self) -> float:
+        """
+        Returns the player's bruised-ness.
+        """
+        return self.data.get("bruised")
+
+    def get_black_eye(self) -> float:
+        """
+        Returns the player's black eye-ness.
+        """
+        return self.data.get("blackEye")
+
+    def get_blacked_out(self) -> float:
+        """
+        Returns the player's blacked out-ness.
+        """
+        return self.data.get("blackedOut")
+
+    def get_numb(self) -> float:
+        """
+        Returns the player's numbness.
+        """
+        return self.data.get("numb")
+
+    def get_poisoned(self) -> float:
+        """
+        Returns the player's poisoned-ness.
+        """
+        return self.data.get("poisoned")
+
+    def get_bleeding(self) -> float:
+        """
+        Returns the player's bleeding rate.
+        """
+        return self.data.get("bleeding")
+
+    def get_well_rested(self) -> float:
+        """
+        Returns the player's well rested-ness.
+        """
+        return self.data.get("wellRested")
+
+    def get_starch_addiction(self) -> float:
+        """
+        Returns the player's starch addiction.
+        """
+        return self.data.get("starchAddiction")
+
+    def get_sync_disk_install(self) -> float:
+        """
+        Returns the player's sync disk install.
+        """
+        return self.data.get("syncDiskInstall")
+
+    def get_blinded(self) -> float:
+        """
+        Returns the player's blindness.
+        """
+        return self.data.get("blinded")
+
+    def is_crouched(self) -> bool:
+        """
+        Returns whether the player is crouched.
+        """
+        return self.data.get("crouched")
+
+    def get_upgrades(self) -> List[dict]:
+        """
+        Returns the player's SyncDisk upgrades.
+        """
+        return self.data.get("upgrades")
+
+    def get_keyring(self) -> List[int]:
+        """
+        Returns the player's keyring.
+        """
+        return self.data.get("keyring")
+
+    def get_buildings(self) -> List[dict]:
+        """
+        Returns buildings in the city.
+        """
+        return self.data.get("buildings")
+
+    def get_companies(self) -> List[dict]:
+        """
+        Returns companies in the city.
+        """
+        return self.data.get("companies")
+
+    def get_evidence(self) -> List[dict]:
+        """
+        Returns evidence.
+        """
+        return self.data.get("evidence")
+
+    def get_guestPasses(self) -> List[dict]:
+        """
+        Returns guest passes.
+        """
+        return self.data.get("guestPasses")
+
+    def get_rooms(self) -> List[dict]:
+        """
+        Returns rooms.
+        """
+        return self.data.get("rooms")
+
+    def get_removedCityData(self) -> List[int]:
+        """
+        Returns removed city data.
+        """
+        return self.data.get("removedCityData")
+
+    def get_citizens(self) -> List[dict]:
+        """
+        Returns citizens.
+        """
+        return self.data.get("citizens")
+
+    def get_doors(self) -> List[dict]:
+        """
+        Returns doors.
+        """
+        return self.data.get("doors")
+
+    def get_active_cases(self) -> List[dict]:
+        """
+        Returns the active cases.
+        """
+        return self.data.get("activeCases")
+
+    def get_crimescenes(self) -> List[dict]:
+        """
+        Returns the crime scenes.
+        """
+        return self.data.get("crimeSceneCleanup")
+
+    def get_passcodes(self) -> List[dict]:
+        """
+        Returns the passcodes.
+        """
+        return self.data.get("passcodes")
+
+    def get_murder_id(self) -> int:
+        """
+        Returns the murder ID of the save file.
+        """
+        return self.data.get("assignMurderID")
+
+    def get_murders(self) -> List[dict]:
+        """
+        Returns the list of murders
+        """
+        return self.data.get("murders")
+
+    def is_murder_routine_active(self) -> bool:
+        """
+        Returns whether the murderer's routine is active.
+        """
+        return self.data.get("murderRoutineActive")
